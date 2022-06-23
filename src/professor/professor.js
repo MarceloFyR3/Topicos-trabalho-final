15317b27d83c067f7397f8eab22e3ed9f31ccd30
marcelodrumand <t_k2.marcelo.drumand@grupoboticario.com.br>
2022-06-23 14:07:55 -0300

    professor - upload

--- src/professor/professor.js
+++ src/professor/professor.js
@@ -8,9 +8,32 @@ $(document).ready(function () {
     }
     carregaDisciplina();
     carregarAlunosLancarNotas();
-
-    console.log(alunos)
 });
+
+const fileInput = document.querySelector("#fileInput");
+
+const uploadFile = file => {
+  console.log("Uploading file...");
+  const API_ENDPOINT = "https://file.io";
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);      
+    }
+  };
+  formData.append("file", file);
+  request.send(formData);
+  let msg = "Arquivo enviado com sucesso!!! "
+  alert(msg);
+};
+
+fileInput.addEventListener("change", event => {
+    const files = event.target.files;
+    uploadFile(files[0]);
+  });
 
 function carregaDisciplina() {
     var select = document.getElementById("sltDisciplina"),
